033bd10fea42e8293c0ff0de00f1f7c5fce3b9a9
AdaoBJr <adaobjr@gmail.com>
2022-03-25 22:12:35 -0400

    feat: ✨ created startServer

---
+++ src/server/startServer.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import { router } from '../routers';
+import ApiError from 'src/errors';
+
+const { PORT } = process.env;
+
+const startServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+
+  app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApiError) {
+      return response.status(error.status).json({ message: error.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${error.message}`,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🔥 Server Running on http://localhost:${PORT}`);
+  });
+};
+
+export { startServer };
